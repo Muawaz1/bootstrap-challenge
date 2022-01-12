c720cd9cc639de73105c6b0e2b03033cee7c4aa9
muawaz <muawaz57@gmail.com>
2022-01-12 05:26:56 +0000

    changes

--- dist/custome.6ee97894.js
+++ dist/custome.6ee97894.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./../images/bg-simplify-section-desktop.svg":[["bg-simplify-section-desktop.b189dfc7.svg","../images/bg-simplify-section-desktop.svg"],"../images/bg-simplify-section-desktop.svg"],"_css_loader":"../../../.nvm/versions/node/v16.13.1/lib/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../../.nvm/versions/node/v16.13.1/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./../images/bg-tablet-pattern.svg":[["bg-tablet-pattern.c02b504f.svg","../images/bg-tablet-pattern.svg"],"../images/bg-tablet-pattern.svg"],"_css_loader":"../../../.nvm/versions/node/v16.13.1/lib/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../../.nvm/versions/node/v16.13.1/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45025" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38459" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-import * as bootstrap from 'bootstrap';
+
+let hamburger = document.querySelector(".navbar-toggler");
+let collapse = document.querySelector(".collapsed");
+let headerOverlay = document.querySelector(".overlay");
+console.log('helooo', hamburger )
+// function toggleHamburger() {
+// 	hamburger.classList.toggle("header-overlay");
+// 	console.log('helooo', hamburger )
+// }
+
+hamburger.addEventListener('click', () => {
+	console.log("heloo")
+
+		headerOverlay.classList.toggle("header-overlay");
+	
+})
